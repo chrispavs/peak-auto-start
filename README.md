df460d03610ac5c3da778e11e1966f552de8ef8f
Chris Pavlopoulos <pavlopouloschris2@gmail.com>
2025-07-16 20:47:45 -0500

    hooray: init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AutoStart
+
+Automatically load into a lobby when the game starts. Save time clicking buttons! Online by default. Solo can be configured in the config.
---
+++ src/AutoStart/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace AutoStart;
+
+class ChrisPavsAutoStartPatches
+{
+    private static bool started = false;
+
+    [HarmonyPatch(typeof(MainMenu), nameof(MainMenu.Update))]
+    [HarmonyPrefix]
+    private static void MainMenu_Update(MainMenu __instance)
+    {
+        if (!started)
+        {
+            started = true;
+            if (Plugin.offlineModeConfig?.Value ?? false)
+            {
+                Plugin.Log.LogInfo($"Launching in offline (solo) mode.");
+                __instance.PlaySoloClicked();
+            }
+            else
+            {
+                Plugin.Log.LogInfo($"Launching in online (multiplayer) mode.");
+                __instance.GoToAirport();
+            }
+        }
+    }
+}
+
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; private set; } = null!;
+    internal static ConfigEntry<bool>? offlineModeConfig;
+
+    private void Awake()
+    {
+        Log = Logger;
+
+        // Log our awake here so we can see it in LogOutput.log file.
+        Log.LogInfo($"Plugin {Name} is loaded!");
+
+        // Apply our patches.
+        Harmony.CreateAndPatchAll(typeof(ChrisPavsAutoStartPatches));
+
+        // Create config options.
+        offlineModeConfig = Config.Bind("Settings", "OfflineMode", false, "If true, the game will start and automatically load into an offline lobby. If false, the game will start and automatically load into an online lobby.");
+    }
+}
